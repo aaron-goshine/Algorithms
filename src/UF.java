f49cafe6aca3c66fe3e5891fd81c72b4b24984b4
Goshine <greenlig@gmail.com>
2015-09-07 08:22:44 +0100

    init commit

---
+++ src/UF.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Aaron.goshine on 07/09/15.
+ */
+public class UF {
+    public boolean connected(int p, int q) {
+        return true;
+    }
+
+    public void union(int p, int q) {
+
+    }
+}
