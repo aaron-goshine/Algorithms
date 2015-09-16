54b1c9cbfbfd929586b9f15df58c21337b9b6a97
Goshine <greenlig@gmail.com>
2015-09-16 22:17:51 +0100

    Added binary search

---
+++ src/BinarySearch.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Aaron.goshine on 13/09/15.
+ */
+public class BinarySearch {
+    public static void main(String [] args) {
+
+    }
+
+    public static int search (int[] a, int key){
+        int lo = 0, hi = a.length - 1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+}
