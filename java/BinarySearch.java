77f01510a697159432554cab2e25885cf8f21a08
Aaron Goshine <greenlig@gmail.com>
2017-02-28 20:12:35 +0000

    Refactor(Code): Reoganised code
    - Rename Java directory
    - Created Python directory

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -10,9 +10,13 @@ public class BinarySearch {
         int lo = 0, hi = a.length - 1;
         while (lo <= hi){
             int mid = lo + (hi - lo) / 2;
-            if (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
-            else return mid;
+            if (key < a[mid]) {
+              hi = mid - 1;
+            } else if (key > a[mid]){
+              lo = mid + 1;
+            } else {
+              return mid;
+            }
         }
         return -1;
     }
