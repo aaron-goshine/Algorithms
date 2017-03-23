843c05cb1c3f1beda2a81140b54c9c32906c0096
Aaron Goshine <greenlig@gmail.com>
2017-03-23 14:46:38 +0000

    docs(Python): add docs to binary_search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,9 +1,21 @@
+'''
+A simple implementation of binary search in python
+'''
+
 
 def binary_search(data, item):
+    '''
+    binary_search, searches a list of items
+    and returns the index of the result if found or
+    None
+    @param data [] - a list of items to search
+    @param item - item to search for
+    @return number | None - the index at which the item was found
+    '''
     low = 0
     high = len(data)
 
-    while(low <= high):
+    while low <= high:
         mid = (low + high) // 2
         current = data[mid]
         if item == current:
